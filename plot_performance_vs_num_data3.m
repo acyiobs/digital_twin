8794572c8e30beddec878bdf10689645d6e71759
Shuaifeng Jiang <jshuaifeng.ece@gmail.com>
2022-10-10 02:00:31 -0700

    ML add new feature
    Train model function and scripts are rebased
    Add feature: train on real/synth and test on real. Training data points = 10~200
    Repeat simulation for smooth curves

--- plot_performance_vs_num_data3.m
+++ plot_performance_vs_num_data3.m
@@ -1,7 +1,12 @@
-load('all_acc_train_on_real3.mat');
-load('all_pwr_train_on_real3.mat');
-load('all_pwr_train_on_synth3.mat');
-load('all_acc_train_on_synth3.mat');
+load('all_acc_train_on_real4.mat');
+load('all_pwr_train_on_real4.mat');
+load('all_pwr_train_on_synth4.mat');
+load('all_acc_train_on_synth4.mat');
+
+all_acc_train_on_real4 = mean(all_acc_train_on_real4, 2);
+all_pwr_train_on_real4 = mean(all_pwr_train_on_real4, 2);
+all_pwr_train_on_synth4 = mean(all_pwr_train_on_synth4, 2);
+all_acc_train_on_synth4 = mean(all_acc_train_on_synth4, 2);
 
 num_data = 10:10:200;
 % plot(num_data, all_acc_train_on_real3(1, i), '--s', "Color", "#0072BD");
@@ -13,13 +18,13 @@ num_data = 10:10:200;
 % plot(num_data, all_pwr_train_on_synth3(1, i), '-o', "Color", "#A2142F");
 % grid on
 
-plot(num_data, all_acc_train_on_real3(2, :), '--s', "Color", "#0072BD");
+plot(num_data, all_acc_train_on_real4(2, :), '--s', "Color", "#0072BD");
 hold on
-plot(num_data, all_pwr_train_on_real3(2, :), '-o', "Color", "#0072BD");
+plot(num_data, all_pwr_train_on_real4(2, :), '-o', "Color", "#0072BD");
 hold on
-plot(num_data, all_acc_train_on_synth3(2, :), '--s', "Color", "#A2142F");
+plot(num_data, all_acc_train_on_synth4(2, :), '--s', "Color", "#A2142F");
 hold on
-plot(num_data, all_pwr_train_on_synth3(2, :), '-o', "Color", "#A2142F");
+plot(num_data, all_pwr_train_on_synth4(2, :), '-o', "Color", "#A2142F");
 grid on
 
 xlabel('Number of Training Data Points');
--- python/train_model_real.py
+++ python/train_model_real.py
@@ -8,28 +8,15 @@ import datetime
 from scipy.io import savemat
 from torch.utils.data import DataLoader
 from train_model import train_model
-import data_feed_real
-import data_feed_synth
-
-
-def train_model_synth(
-    num_epoch=80,
-    if_writer=False,
-):
-    test_loss, test_acc, test_pwr, predictions, raw_predictions, true_label, PATH = train_model(
-        train_loader=train_loader,
-        val_loader=val_loader,
-        test_loader=test_loader,
-        comment=comment,
-        num_classes=16,
-        num_epoch=num_epoch,
-        if_writer=if_writer,
-    )
-
-    return test_loss, test_acc, test_pwr, predictions, raw_predictions, true_label, PATH
+from data_feed import DataFeed
 
 
 if __name__ == "__main__":
+    real_beam_pwr_path = 'real_beam_pwr.mat'
+    real_pos_path = 'ue_relative_pos.mat'
+    synth_beam_pwr_path = 'synth_beam_power.mat'
+    synth_pos_path = 'synth_UE_loc.mat'
+
     torch.manual_seed(2022)
     now = datetime.datetime.now().strftime("%H_%M_%S")
     date = datetime.date.today().strftime("%y_%m_%d")
@@ -41,46 +28,47 @@ if __name__ == "__main__":
     all_acc = []
     all_pwr = []
 
-    # max_data_point = 1928
-    # for percentage in [0.05, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0]:
-    #     num_data_point = int(max_data_point * percentage)
-    
-    #     train_loader = DataLoader(data_feed_real.DataFeed(mode='train', num_data_point=num_data_point), batch_size, shuffle=True)
-    #     val_loader = DataLoader(data_feed_real.DataFeed(mode='test'), val_batch_size, shuffle=False)
-    #     test_loader = DataLoader(data_feed_real.DataFeed(mode='test'), val_batch_size, shuffle=False)
-
-    #     print('percentage: '+ str(percentage))
-    #     test_loss, test_acc, test_pwr, predictions, raw_predictions, true_label, PATH = train_model_synth(num_epoch, if_writer=False)
-
-    #     all_acc.append(test_acc)
-    #     all_pwr.append(test_pwr)
-    #     # print(test_loss)
-    #     # print(test_acc)
-    #     # print(test_pwr)
-    # all_acc = np.stack(all_acc, -1)
-    # all_pwr = np.stack(all_pwr, -1)
-
     max_data_point = 1928
     for num_data_point in np.arange(10, 210, 10): #[100, 150, 200, 300, 400, 500, 600, 700, 800]
-    
-        train_loader = DataLoader(data_feed_real.DataFeed(mode='train', num_data_point=num_data_point), batch_size, shuffle=True)
-        val_loader = DataLoader(data_feed_real.DataFeed(mode='test'), val_batch_size, shuffle=False)
-        test_loader = DataLoader(data_feed_real.DataFeed(mode='test'), val_batch_size, shuffle=False)
+        acc = []
+        pwr = []
+        for _ in range(30):
+            rand_state = int(torch.randint(low=1, high=2000, size=(1,)))
 
-        print('Number of data points : '+ str(num_data_point))
-        test_loss, test_acc, test_pwr, predictions, raw_predictions, true_label, PATH = train_model_synth(num_epoch, if_writer=False)
+            dataset_real_train = DataFeed(pos_path=real_pos_path, beam_pwr_path=real_beam_pwr_path, rand_state=rand_state, mode='train', num_data_point=num_data_point)
+            dataset_real_test = DataFeed(pos_path=real_pos_path, beam_pwr_path=real_beam_pwr_path, rand_state=rand_state, mode='test')
+            dataset_synth_train = DataFeed(pos_path=synth_pos_path, beam_pwr_path=synth_beam_pwr_path, rand_state=rand_state, mode='train', num_data_point=num_data_point)
+            dataset_synth_test = DataFeed(pos_path=synth_pos_path, beam_pwr_path=synth_beam_pwr_path, rand_state=rand_state, mode='test')
 
-        all_acc.append(test_acc)
-        all_pwr.append(test_pwr)
-        # print(test_loss)
-        # print(test_acc)
-        # print(test_pwr)
+            real_train_loader = DataLoader(dataset_real_train, batch_size=batch_size, shuffle=True)
+            real_test_loader = DataLoader(dataset_real_test, val_batch_size, shuffle=False)
+            synth_train_loader = DataLoader(dataset_synth_train, batch_size=batch_size, shuffle=True)
+            synth_test_loader = DataLoader(dataset_synth_test, val_batch_size, shuffle=False)
+
+            print('Number of data points : '+ str(num_data_point))
+            test_loss, test_acc, test_pwr, predictions, raw_predictions, true_label, PATH = train_model(
+                train_loader=real_train_loader,
+                val_loader=real_test_loader,
+                test_loader=real_test_loader,
+                comment=comment,
+                num_classes=16,
+                num_epoch=num_epoch,
+                if_writer=False,
+            )
+            acc.append(test_acc)
+            pwr.append(test_pwr)
+        acc = np.stack(acc, -1)
+        pwr = np.stack(pwr, -1)
+
+        all_acc.append(acc)
+        all_pwr.append(pwr)
+
     all_acc = np.stack(all_acc, -1)
     all_pwr = np.stack(all_pwr, -1)
 
     print(all_acc)
     print(all_pwr)
-    savemat('all_acc_train_on_real3.mat', {'all_acc_train_on_real3': all_acc})
-    savemat('all_pwr_train_on_real3.mat', {'all_pwr_train_on_real3': all_pwr})
+    savemat('all_acc_train_on_real4.mat', {'all_acc_train_on_real4': all_acc})
+    savemat('all_pwr_train_on_real4.mat', {'all_pwr_train_on_real4': all_pwr})
     print('done')
 
